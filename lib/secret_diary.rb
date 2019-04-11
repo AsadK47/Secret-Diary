4db79456c5910b43568f9284e7d7de3f3539d382
Asad Khan <asad_khan47@hotmail.co.uk>
2019-04-11 12:43:01 +0100

    Added empty line after guard clause as per rubocops automatic fix

--- lib/secret_diary.rb
+++ lib/secret_diary.rb
@@ -6,11 +6,13 @@ class Diary
 
   def add_entry(entry)
     fail "Diary is locked" if @lock
+
     add_to_diary(entry)
   end
 
   def retrieve_entries
     fail "Diary is locked" if @lock
+
     puts @entry
   end
 
